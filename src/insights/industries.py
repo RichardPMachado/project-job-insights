7087858568abf40aec325e7d3260814a535f20dc
RichardPMachado <richardmachado2211@gmail.com>
2023-04-29 17:19:36 -0300

    feat[requisito 7][filter_by_industry}: A função retorna uma lista de dicionários com todos os empregos onde a coluna industry corresponde ao parâmetro industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -39,4 +39,5 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    # print([job for job in jobs if (job["industry"] == industry)])
+    return [job for job in jobs if (job["industry"] == industry)]
